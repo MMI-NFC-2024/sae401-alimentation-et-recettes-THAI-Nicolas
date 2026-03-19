54f886bc6dcfb8ef700791f2f57099fbfe4942b0
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2026-03-19 16:01:57 +0100

    Mise en place d'une authentification sécurisée via la collection "users" de PocketBase

---
+++ src/pages/auth/logout.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from "astro";
+
+//Route POST pour se déconnecter : on efface les données d'authentification de PocketBase et on redirige vers la page d'accueil
+export const POST: APIRoute = async ({ locals, redirect }) => {
+  locals.pb.authStore.clear();
+  return redirect("/");
+};
+
+//Protection de la route GET pour éviter que les utilisateurs accèdent à cette page via une requête GET (ex: en tapant l'URL dans la barre d'adresse).
+export const GET: APIRoute = async ({ redirect }) => {
+  return redirect("/auth/login");
+};
