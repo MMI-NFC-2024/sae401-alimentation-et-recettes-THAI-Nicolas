991c61dd7739cb4f283b36e86e8297e5f24c9e55
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2026-03-20 20:09:37 +0100

    Page Nos Conseils (les articles), création des fonctions dans le service.ts

--- src/lib/services/articles.service.ts
+++ src/lib/services/articles.service.ts
@@ -1,6 +1,6 @@
 import type { ArticlesResponse, TypedPocketBase } from "../../pocketbase-types";
 
-export interface LatestArticlesResult {
+export interface ArticlesCollection {
   articles: ArticlesResponse[];
   fetchError: boolean;
 }
@@ -8,7 +8,7 @@ export interface LatestArticlesResult {
 export async function getLatestArticles(
   pb: TypedPocketBase,
   limit = 3,
-): Promise<LatestArticlesResult> {
+): Promise<ArticlesCollection> {
   try {
     const articlesPage = await pb.collection("articles").getList(1, limit, {
       sort: "-created",
@@ -30,3 +30,101 @@ export async function getLatestArticles(
     };
   }
 }
+
+export async function getAllArticles(
+  pb: TypedPocketBase,
+): Promise<ArticlesCollection> {
+  try {
+    const articlesPage = await pb.collection("articles").getList(1, 100, {
+      sort: "-created",
+    });
+
+    return {
+      articles: articlesPage.items,
+      fetchError: false,
+    };
+  } catch (error) {
+    console.error(
+      "[articles.service] Impossible de recuperer les articles PocketBase",
+      error,
+    );
+
+    return {
+      articles: [],
+      fetchError: true,
+    };
+  }
+}
+
+export async function getArticleBySlug(
+  pb: TypedPocketBase,
+  slug: string,
+): Promise<ArticlesCollection> {
+  try {
+    const articlesPage = await pb.collection("articles").getOne(slug);
+    return {
+      articles: [articlesPage],
+      fetchError: false,
+    };
+  } catch (error) {
+    console.error(
+      "[articles.service] Impossible de recuperer l'article PocketBase",
+      error,
+    );
+
+    return {
+      articles: [],
+      fetchError: true,
+    };
+  }
+}
+
+// export async function getArticlesByType(
+//   pb: TypedPocketBase,
+//   type: string,
+// ): Promise<ArticlesCollection> {
+//   try {
+//     const articlesPage = await pb.collection("articles").getFullList({
+//       sort: "-created",
+//       filter: `type="${type}"`,
+//     });
+//     return {
+//       articles: articlesPage,
+//       fetchError: false,
+//     };
+//   } catch (error) {
+//     console.error(
+//       "[articles.service] Impossible de recuperer les articles PocketBase par type",
+//       error,
+//     );
+//     return {
+//       articles: [],
+//       fetchError: true,
+//     };
+//   }
+// }
+
+// export async function getArticlesByTitle(
+//   pb: TypedPocketBase,
+//   title: string,
+// ): Promise<ArticlesCollection> {
+//   try {
+//     const articlesPage = await pb.collection("articles").getFullList({
+//       sort: "-created",
+//       filter: `titre="${title}"`,
+//     });
+//     return {
+//       articles: articlesPage,
+//       fetchError: false,
+//     };
+//   } catch (error) {
+//     console.error(
+//       "[articles.service] Impossible de recuperer l'article PocketBase par titre",
+//       error,
+//     );
+//     return {
+//       articles: [],
+//       fetchError: true,
+//     };
+//   }
+// }
