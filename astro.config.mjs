0857ecb8b2d2f2bb7b85446549482d06c8f6273b
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2026-03-23 21:45:02 +0100

    Configuration du astro config pour accepter les requetes de formulaire venant de slurpy.nicolas-thai.fr (sécurité CSRF de Astro et des Actions)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,34 @@ import node from "@astrojs/node";
 import vue from "@astrojs/vue";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
+// On utilise import.meta.env (syntaxe Astro) ou on type process pour éviter l'erreur
+// @ts-ignore
+const SITE_URL = process.env.SITE_URL || "https://slurpy.nicolas-thai.fr";
+
 export default defineConfig({
-  integrations: [vue()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: SITE_URL,
+
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
+
+  security: {
+    checkOrigin: true,
+    // On transforme la string en objet pour plaire à TypeScript
+    allowedDomains: [
+      {
+        hostname: "slurpy.nicolas-thai.fr",
+      },
+    ],
+  },
+
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ["slurpy.nicolas-thai.fr"],
+    },
+  },
 });
