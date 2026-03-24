6ace6029c2b63f46e1fe49909870cfbb33a5bd99
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2026-03-24 14:30:42 +0100

    Relecture du code et commentaire pour améliorer la lisibilité : L'authentification et l'Oauth

--- src/pages/auth/oauth/[provider].ts
+++ src/pages/auth/oauth/[provider].ts
@@ -1,3 +1,4 @@
+// Route pour déparrer le flux d'authentification OAuth : redirige vers la page d'auth du provider avec les bons paramètres
 import type { APIRoute } from "astro";
 import { handleOAuthStart } from "../../../lib/utils/oauth";
 
--- src/pages/auth/oauth/[provider]/callback.ts
+++ src/pages/auth/oauth/[provider]/callback.ts
@@ -1,4 +1,5 @@
-//Gè
+//Gère le retour du provider OAuth après que l'utilisateur se soit authentifié : vérifie les paramètres,
+// valide la connexion avec PocketBase et redirige vers la page d'accueil ou la page de retour spécifiée
 
 import type { APIRoute } from "astro";
 import { handleOAuthCallback } from "../../../../lib/utils/oauth";
